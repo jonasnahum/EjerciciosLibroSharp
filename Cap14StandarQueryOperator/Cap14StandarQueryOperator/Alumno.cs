39c3894c6654cef7c84d147081435b0cb4776987
jonasnahum <jonasnahum@gmail.com>
2014-08-01 12:36:56 -0500

    cap14 analizado

--- Cap14StandarQueryOperator/Cap14StandarQueryOperator/Alumno.cs
+++ Cap14StandarQueryOperator/Cap14StandarQueryOperator/Alumno.cs
@@ -25,37 +25,69 @@ namespace Cap14StandarQueryOperator
         public string Nombre { get; set; }
         public int Edad { get; set; }
         public decimal Promedio { get; set; }
-        public Alumno(string nombre, int edad,decimal promedio, string nombrePadre)
+        public int IdSalon { get; set; }
+        public Alumno(string nombre, int edad,decimal promedio, string nombrePadre, int idSalon)
         {
             Nombre = nombre;
             Edad = edad;
             Promedio = promedio;
             Padre = new PadreDeFamilia(nombrePadre);
+            IdSalon = idSalon;
         }
         public static IEnumerable<Alumno> ObtenerAlumnos()//de typo IEnumerable generico o de <T>
         {
             IList<Alumno> alumnos = new List<Alumno>();//,,IEnumerable, List, e IList son compatibles.List<T> implementa IEnumerable<T>
-            alumnos.Add(new Alumno("jonas", 28, 8.4M,"jose"));
-            alumnos.Add(new Alumno("pedro", 20, 9M,"maria"));
-            alumnos.Add(new Alumno("juan", 26, 7.4M,"juana"));
-            alumnos.Add(new Alumno("mary", 18, 6.4M,"Marycruz"));
-            alumnos.Add(new Alumno("lupe", 16, 8M,"guadalupe"));
-            alumnos.Add(new Alumno("chole", 30, 5M,"soledad"));
-            alumnos.Add(new Alumno("amon", 22, 7M,"nenita"));
-            alumnos.Add(new Alumno("edwin", 15, 7.64M,"ceci"));
-            alumnos.Add(new Alumno("sandra", 16, 8.6M,"sandra"));
-            alumnos.Add(new Alumno("vero", 32, 6.5M,"maria"));
-            alumnos.Add(new Alumno("julieta", 28, 8.4M,"vero"));
-            alumnos.Add(new Alumno("diego", 14, 8.4M,"Nury"));
-            alumnos.Add(new Alumno("paul", 15, 6.5M,"estela"));
-            alumnos.Add(new Alumno("paty", 8, 7.8M,"yolanda"));
-            alumnos.Add(new Alumno("hugo", 23, 3.0M,"lucrecia"));
+            alumnos.Add(new Alumno("jonas", 28, 8.4M,"jose",1));
+            alumnos.Add(new Alumno("jonas", 20, 9M,"maria",1));
+            alumnos.Add(new Alumno("juan", 26, 7.4M,"juana",1));
+            alumnos.Add(new Alumno("mary", 18, 6.4M,"Marycruz",1));
+            alumnos.Add(new Alumno("lupe", 16, 8M,"guadalupe",1));
+            alumnos.Add(new Alumno("chole", 30, 5M,"soledad",2));
+            alumnos.Add(new Alumno("amon", 22, 7M,"nenita",2));
+            alumnos.Add(new Alumno("edwin", 15, 7.64M,"ceci",2));
+            alumnos.Add(new Alumno("sandra", 16, 8.6M,"sandra",2));
+            alumnos.Add(new Alumno("vero", 32, 6.5M,"maria",2));
+            alumnos.Add(new Alumno("julieta", 28, 8.4M,"vero",3));
+            alumnos.Add(new Alumno("diego", 14, 8.4M,"Nury",3));
+            alumnos.Add(new Alumno("paul", 15, 6.5M,"estela",3));
+            alumnos.Add(new Alumno("paty", 8, 7.8M,"yolanda",3));
+            alumnos.Add(new Alumno("hugo", 23, 3.0M,"lucrecia",3));
 
-            return alumnos;
+            alumnos.Add(new Alumno("solovino", 5, 7.8M, "Toby", 13));
+            alumnos.Add(new Alumno("pantera", 23, 3.0M, "wera", 13));
+            return alumnos;//regresa una lista de objetos de tipo IList<Alumno>
         }
         public override string ToString()//el base por default es el fully qualifyed name, este override regresa los datos del alumno.
         {
             return string.Format("{0} {1} {2}", Nombre, Edad, Promedio);
         }
     }
+    public class Salon
+	{
+        public int Grado { get; set; }
+        public string Grupo { get; set; }
+        public int Id { get; set; }
+        public Salon(int grado, string grupo, int id)
+        {
+            Grado = grado;
+            Grupo = grupo;
+            Id = id;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}° {1}", Grado, Grupo);
+        }
+        public static IEnumerable<Salon> ObtenerSalones() //IEnumerable, la interfase que implementan todas las listas, de tipo IEnumerable<Salon>.
+        {
+            Salon salon1 = new Salon(1, "A", 1);//crea objetos.
+            Salon salon2 = new Salon(2, "B", 2);
+            Salon salon3 = new Salon(3, "C", 3);
+            Salon salon4 = new Salon(4, "C", 4);
+
+            return new Salon[] { salon1, salon2, salon3,salon4   };//regresa un array de objetos, de tipo IEnumerable<Salon>.
+        }
+
+	}
 }
+
--- Cap14StandarQueryOperator/Cap14StandarQueryOperatorTest/UnitTest1.cs
+++ Cap14StandarQueryOperator/Cap14StandarQueryOperatorTest/UnitTest1.cs
@@ -4,6 +4,7 @@ using Cap14StandarQueryOperator;
 using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
+using System.Diagnostics;
 
 namespace Cap14StandarQueryOperatorTest
 {
@@ -106,18 +107,17 @@ namespace Cap14StandarQueryOperatorTest
         }
         [TestMethod]
         public void WhereTest()//ocupa el using System.Linq
-        {//se usa para filtrar datos.
+        {//where se usa para filtrar datos.
             IEnumerable<Alumno> lista = Alumno.ObtenerAlumnos();//se asignan todos los alumnos a la variable lista.
             IEnumerable<Alumno> alumnosConInicialA;//se declara otra variable.
-            Func<Alumno, bool> filtro = a => a.Nombre.ToLower().StartsWith("a");//se declara un delegado y se le asigna un metodo.
+            Func<Alumno, bool> filtro = a => a.Nombre.ToLower().StartsWith("a");//se declara un delegado y se le asigna un metodo.//para ser mas practico, esto deberia ir como parametro en el where.
             alumnosConInicialA = lista.Where(filtro);
-
             System.Diagnostics.Debug.Print("---------------Empiezan con 'a'---------------");            
-            Print(alumnosConInicialA);
+            Print(alumnosConInicialA);//Print es un metodo que recibe un IEnumerable<T>
 
             System.Diagnostics.Debug.Print("---------------Reprobados---------------");
             IEnumerable<Alumno> alumnosReprobados;
-            alumnosReprobados = lista.Where(a => a.Promedio < 6);//where recibe como parametro un delegado. que en este caso es de tipo alumno y regresa un bool.
+            alumnosReprobados = lista.Where(a => a.Promedio < 6);//where recibe como parametro un delegado. que en este caso es de tipo alumno y regresa un bool porque va diciendo si es true o false que el promedio es>6, a su vez tambien regresa de ahi un IEnumerable.
             Print(alumnosReprobados);
 
             System.Diagnostics.Debug.Print("---------------Adultos, Aprobados, Empiece o termine con vocal---------------");
@@ -125,30 +125,174 @@ namespace Cap14StandarQueryOperatorTest
             Print(lista.Where(a => a.Edad > 18 && a.Promedio >= 6 && (vocales.Contains(a.Nombre.Substring(0, 1)) || vocales.Contains(a.Nombre.Substring(a.Nombre.Length-1,1)))));
 
             System.Diagnostics.Debug.Print("---------------Menores Aprobados---------------");
-            Print(lista.Where(a => a.Edad < 18 && a.Promedio >= 6));//where recibe un metodo, un lambda, a su vez el lambda recibe un Alumno y regresa un bool.el where regresa un IEnumerable con los elementos filtrados.
+            Print(lista.Where(a => a.Edad < 18 && a.Promedio >= 6));//where recibe un metodo, un lambda, a su vez el lambda recibe un Alumno y regresa un bool.el where regresa un IEnumerable con los elementos filtrados y lo pasa a lista, y lista lo pasa a print.
 
         }
         [TestMethod]
         public void SelectTest()//ocupa el using System.Linq
         {// sirve para hacer proyecciones, la proyeccion es convertir un IEnumerable de un tipo en otro IEnumerable de otro tipo diferente.
-            IEnumerable<Alumno> lista = Alumno.ObtenerAlumnos();
-            IEnumerable<int> longitudNombres;
-            longitudNombres = lista.Select(a => a.Nombre.Length);
-            Print(longitudNombres);
+            IEnumerable<Alumno> lista = Alumno.ObtenerAlumnos();//se declara variable de tipo IEnumerable<Alumno>
+            IEnumerable<int> longitudNombres;//es un tipo IEnumerable diferente.
+            longitudNombres = lista.Select(a => a.Nombre.Length);//de lado izquierdo es IEnumerable<Int>, y del lado derecho trabaja con IEnumarable<Alumno>, hace una proyeccion.
+            Print(longitudNombres);//Print() es un metodo que recibe un IEnumerable<T>.
 
-            IEnumerable<PadreDeFamilia> papasReprobados;
-            papasReprobados = lista.Where(a => a.Promedio < 6).Select(a=>a.Padre);//de una coleccion de alumnos reprobados se convierte a una coleccion de padres de familia, a la conversion se le llama proyeccion.
+            IEnumerable<PadreDeFamilia> papasReprobados;//se declara una variable y su tipo.
+            papasReprobados = lista.Where(a => a.Promedio < 6).Select(a=>a.Padre);//de una coleccion de alumnos reprobados se convierte a una coleccion de padres de familia, a la conversion se le llama proyeccion.primero ejecuta lista hasta el punto, luego dentro de los parentesis hasta el punto,  luego el select .
             System.Diagnostics.Debug.Print("---------------Papas de reprobados---------------");
             Print(papasReprobados);
 
             IEnumerable<char> inicialNombreAlumno;
-            inicialNombreAlumno = lista.Select(a => a.Nombre[0]);//el Select esta convirtiendo.
+            inicialNombreAlumno = lista.Select(a => a.Nombre[0]);//el string se comporta como un char
             System.Diagnostics.Debug.Print("---------------Iniciales de Alumnos---------------");
             Print(inicialNombreAlumno);
 
             var nombreAlumnoNombrePadre = lista.Select(a => new { NombrePadre = a.Padre.Nombre, NombreAlumno = a.Nombre });//proyeccion a un tipo anonimo, convierte una lista de tipo Alumno a un tipo anonimo.
             System.Diagnostics.Debug.Print("---------------Proyeccion a un Tipo Anonimo---------------");
             Print(nombreAlumnoNombrePadre);
+
+        }
+
+        [TestMethod]
+        public void Paralel()
+        {//pone a trabajar todos los procesadores para hacer la misma operacion y divide el trabajo y lo hace mas rapido.
+            List<Alumno> lista = Alumno.ObtenerAlumnos().ToList();//ObtenerAlumnos es de tipo IEnumerable, lo convierte a lista y lo guarda en variable lista.
+            Action<Alumno> imprimirAlumno = a =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    try
+                    {
+                        throw new NotImplementedException();
+                    }
+                    catch
+                    {
+                    }
+                }
+            };//un lambda que tira errores, para gastar memoria y luego contar el tiempo que tarda en tirar errores.
+
+            DateTime inicio = DateTime.Now;//se registra el tiempo de inicio.
+            lista.ForEach(imprimirAlumno);//a cada elemento en List<Alumno>lista, se le aplica la variable delegado immpmrimirAlumno que recibe un Alumno, porque cada elemto es de tipo Alumno, y todos juntos son list<Alumno>.
+            TimeSpan tiempo = inicio - DateTime.Now;//la variable tiempo, saca la diferencia de tiempos, el span de lo que duro la operacion throw.
+            Debug.Print("{0}: {1}", "sin parallel", tiempo.Milliseconds);
+
+            inicio = DateTime.Now;
+            lista.AsParallel().ForAll(imprimirAlumno);//a la lista se le aplica AsParallel, y  luego su metodo forAll imprimirAlumno.
+            tiempo = inicio - DateTime.Now;//se saca la diferencia.
+            Debug.Print("{0}: {1}", "con parallel", tiempo.Milliseconds);
+        }
+        [TestMethod]
+        public void CountTest()
+        {//cuenta los elementos de una lista segun el metodo lambda, regresa un numero, un valor. tiene una sobre carga que recibe un lambda y otra que no recibe , que solo cuenta.
+            int cantidadDeAlumnosAprobados = Alumno.ObtenerAlumnos().Count(a => a.Promedio >= 6);
+            Debug.Print("De {0} alumnos, {1} aprobaron", Alumno.ObtenerAlumnos().Count(), cantidadDeAlumnosAprobados);
+        }
+        [TestMethod]
+        public void AnyTest()
+        {// recorre la lista y regresa un true, si existe la condicion que se paso como lambda, si no lleva parametro el Any, regresa true si la lista contiene por lo menos 1 elemento.
+            if (Alumno.ObtenerAlumnos().Any())//los if llevan entre parentesis un bool.//si obtener alumnos contiene por lo menos 1 elemento.
+            {
+                Debug.Print("esta lista contiene por lo mentos 1 elemento");
+            }
+            if (Alumno.ObtenerAlumnos().Any(a=>a.Promedio<6))//si por lo menos hay un elemento reprobado.
+            {
+                Debug.Print("por lo menos hay un alumno reprobado");
+            }
+        }
+        [TestMethod]
+        public void OtrosMetodosQueOperanSobreIEnumerable() 
+        {//Avarage().saca el promedio de una secuencia, segun su typo.
+            var alumnos = Alumno.ObtenerAlumnos();
+            decimal promedioGeneralDelGpo= alumnos.Average(a => a.Promedio);
+            Debug.Print(promedioGeneralDelGpo.ToString());
+        }
+
+        public bool EsAprobado(Alumno alumno)//este metodo equivale a un lambda.
+        {
+            bool aprobo = alumno.Promedio >= 6;
+            return aprobo;
+        }
+
+        [TestMethod]
+        public void DeferredExecutionTest()
+        {// los query operators se ejecutan hasta el punto en que ejecuta una operacion o se solicita algun dato de la colleccion sobre la que actuan.
+            IEnumerable<Alumno> todosLosAlumnos = Alumno.ObtenerAlumnos();
+            IEnumerable<Alumno> aprobados = todosLosAlumnos.Where(EsAprobado);//aqui el where sabe lo que va y como lo va hacer, pero no se ejecuta.
+
+            int cantidadDeAlumnos = 0;
+            cantidadDeAlumnos = aprobados.Count();//en este momento se ejecuta el where y su parametro.
+        }
+        [TestMethod]
+        public void IEnumerableConvertionTest()
+        {//Un IEnumerable, se convierte facilmente a un array o a un List.
+            IEnumerable<Alumno> alumnos = Alumno.ObtenerAlumnos();//este es un IEnumerable.
+            Alumno[] alumnosArray = alumnos.ToArray();
+            List<Alumno> alumnosList = alumnos.ToList();
+        }
+        [TestMethod]
+        public void OrdenamientoIEnumerable()
+        {//OrderBy.. ordena de a-z o de 1--2,
+         //ThenBy. es utilizado para agregar un segundo criterio de ordenamiento.
+            IEnumerable<Alumno> alumnos = Alumno.ObtenerAlumnos();
+            Debug.Print("--------------------Alumnos en Orden Alfabetico Z-A-----------------------------------");
+            IEnumerable<Alumno> ordenados = alumnos.OrderByDescending(a => a.Nombre);
+            Print(ordenados);
+
+            Debug.Print("-------------------Ordenado por Promedio de Menor a Mayor-----------------------------------");
+            ordenados = alumnos.OrderBy(a => a.Promedio);
+            Print(ordenados);
+
+            Debug.Print("--------------------Ordenado Por Orden Alfabetico y Luego Por Edad-----------------------------------");
+            ordenados = alumnos.OrderBy(a => a.Nombre).ThenBy(a=>a.Edad);//el primer criterio es ordenado, y Then By es un criterio secundario de ordenamiento, si hay dos nombres iguales, pone primero el de menor edad.
+            Print(ordenados);
+        }
+        [TestMethod]
+        public void JoinOperators()
+        {//one to many.//un salon a varios alumnos.
+            IEnumerable<Alumno> todosLosAlumnos = Alumno.ObtenerAlumnos();
+            IEnumerable<Salon> todosLosSalones = Salon.ObtenerSalones();
+            //var grupo = todosLosAlumnos.Join(todosLosSalones,//todos los alumnos hacen join con todos los salones. 
+            //    a => a.IdSalon,//foreing key, porque el id esta es el numero de salon y el salon es una caracteristica externa, que esta en salon no aqui en alumno.
+            //    s => s.Id, //primary key, porque el id identifica a cada uno de los elementos de salon y con base en este, se agrupan los alumnos.
+            //    (a, s) => new
+            //    { //aqui ha mandado todos los elementos que se pudieron juntar con el join, para hacer una proyeccion.,se hace una clase anonyma.
+            //        Alumno = a,
+            //        Salon = s,
+            //    });//la variable grupo, al final tiene un IEnumerable de esta clase anonyma.
+            //Debug.Print("-------------------Inner Join-----------------------------------");
+            //Print(grupo.Select(anonymo=>string.Format("{0}-{1}",anonymo.Alumno.ToString(),anonymo.Salon.ToString())));
+            var grupo = todosLosAlumnos.Join(todosLosSalones,//first secuence(inner)                inner, to join to the firstsecuence.
+                a => a.IdSalon,//OuterKeySelector. a funcion to extract the join key from each element of the first secuence.
+                s=>s.Id,//innerKeySelector a funtion to extract the join key from each element of the second secuence.
+                (a, s) => new//result selector. a function to create a result element from two matching elements.
+                    {
+                        Alumno = a,
+                        Salon = s,
+                    });
+            Debug.Print("-------------------Inner Join-----------------------------------");
+            Print(grupo.Select(anonymo => string.Format("{0}-{1}", anonymo.Alumno.ToString(), anonymo.Salon.ToString())));
+
+            Debug.Print("-------------------Right Join-----------------------------------");            
+            var rightResult = todosLosSalones.GroupJoin(todosLosAlumnos,
+            s => s.Id,
+            a => a.IdSalon,
+            (s, alumnos) => new//hay objetos combinados cada uno tiene salon y alumno. aqui ya esta la proyeccion en la cual cada salon ya tiene sus alumnos.estan agrupados en base a su propiedad comun.
+            {
+                Alumnos = alumnos,
+                Salon = s//right result, la variable, tiene un IEnumerable de objetos de esta clase anonyma, cada objeto, tendra estas dos propiedades.
+            });
+
+            Print(rightResult.Where(it => it.Alumnos.Count() == 0).Select(it=>it.Salon));//buscar en cada objeto de la clase anonyma en donde sus alumnos=0, de esa lista hacer una proyeccion y sacar los salones.
+
+            Debug.Print("-------------------Left Join-----------------------------------");
+            var leftJoint = todosLosAlumnos.GroupJoin(todosLosSalones,
+            a => a.IdSalon,
+            s => s.Id,
+            (a, salones) => new
+            {
+                Salones = salones,
+                Alumno = a
+            });
+            Print(leftJoint.Where(classAnonym => classAnonym.Salones.Count() == 0).Select(classAnonym => classAnonym.Alumno));//solovino y pantera si tienen salon, pero sus Key selector no encuentra relacion con un salon.
         }
     }
 }
