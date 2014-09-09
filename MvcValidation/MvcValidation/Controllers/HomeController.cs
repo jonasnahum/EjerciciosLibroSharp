426cddd6fd13cf06d63a00fc5edbbc4653ee7a08
jonasnahum <jonasnahum@gmail.com>
2014-09-09 18:57:43 -0500

    registrarise en face

---
+++ MvcValidation/MvcValidation/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using MvcValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            //ModelState.AddModelError("", "This is all wrong");
+            //ModelState.AddModelError("Title", "what a terrible name");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(PersonaModel persona)
+        {
+                   return View(persona);
+        }
+    }
+}
---
+++ MvcValidation/MvcValidation/Models/PersonaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcValidation.Models
+{
+    public class PersonaModel
+    {
+        [Required(ErrorMessage="Este campo es obligatorio")]
+        public string Nombre { get; set; }
+        [Range(0,80,ErrorMessage="la edad debe ser entre 0 y 80")]
+        public int Edad { get; set; }
+
+    }
+}
